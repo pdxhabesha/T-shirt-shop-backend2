a6f02f44a1b2ed3a51d32d8a3c2c39186d8a6ad8
sibhat <sibhat.temelso@gmail.com>
2019-03-05 19:36:56 -0800

    add gunicorn

--- tshirt/settings.py
+++ tshirt/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 import django_heroku
 django_heroku.settings(locals())
